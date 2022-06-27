92935ca360edf0f6f1bf0c7c1f2e1bf006973cdd
Mazen-Hasan <e.mazen.hasan@gmail.com>
2022-06-27 18:12:12 +0300

    Calling API

---
+++ app/Http/Traits/CallAPI.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait CallAPI
+{
+    static public function postAPI($apiName, $body){
+        $base_url = env("API_BASE_URL", "http://127.0.0.1:8000/api/");
+        $content_type = env("API_CONTENT_TYPE", "application/json");
+        $accept = env("API_ACCEPT", "application/json");
+        $accept_language = env("API_ACCEPT_LANGUAGE", "1");
+
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $base_url,
+            'headers' => ['Content-Type' => $content_type, "Accept" => $accept, 'Accept-Language'=> $accept_language]
+        ]);
+
+        $res = $client->post($apiName, [
+            'body' => json_encode($body)
+        ]);
+
+        $resJson = (json_decode($res->getBody()->getContents()));
+
+        $result['errCode'] = $resJson->errCode;
+        $result['errMsg'] = $resJson->errMsg;
+        $result['data'] = json_decode(json_encode($resJson->data), true);;
+
+        return $result;
+    }
+}
